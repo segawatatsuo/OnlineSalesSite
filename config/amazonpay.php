bc5b9bc18fcf775da8951ef4318549afbb9af727
segawatatsuo <base@lookingfor.jp>
2025-09-23 14:29:00 +0900

    9-23 14:28

--- config/amazonpay.php
+++ config/amazonpay.php
@@ -11,24 +11,27 @@ return [
     */
 
     'public_key_id' => env('AMAZON_PAY_PUBLIC_KEY_ID', 'SANDBOX-AFZNNIULXIPQCHDKP22S55W4'),
-    
-    'private_key_path' => env('AMAZON_PAY_PRIVATE_KEY_PATH', storage_path('app/keys/amazonpay_private.pem')),
-    
+
+    //'private_key_path' => env('AMAZON_PAY_PRIVATE_KEY_PATH', storage_path('app/keys/amazonpay_private.pem')),
+    'private_key_path' => storage_path('app/keys/' . env('AMAZON_PAY_PRIVATE_KEY_FILE')),
+
     'region' => env('AMAZON_PAY_REGION', 'jp'),
-    
+
     'sandbox' => env('AMAZON_PAY_SANDBOX', true),
-    
+
     'store_id' => env('AMAZON_PAY_STORE_ID', 'amzn1.application-oa2-client.28e60372ee0d42c48e7c1c9036655abb'),
-    
+
     'merchant_id' => env('AMAZON_PAY_MERCHANT_ID', 'A2MQAIFB5IWHUJ'),
-    
+
     'store_name' => env('AMAZON_PAY_STORE_NAME', 'SHOP_NAME'),
-    
+
     /*
     |--------------------------------------------------------------------------
     | Amazon Pay URLs
     |--------------------------------------------------------------------------
     */
-    
+
     'checkout_js_url' => env('AMAZON_PAY_CHECKOUT_JS_URL', 'https://static-fe.payments-amazon.com/checkout.js'),
+
+
 ];
